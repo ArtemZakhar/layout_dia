d7a1dfe60eeafbe4c69423c1bc3ba210d3cdaf83
Artem Zakharchuk <artemzakharchuk@gmail.com>
2024-03-21 14:45:58 +0200

    make build

--- src/scripts/components/theme-switcher.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const page = document.querySelector('.js-page');
-const switcher = document.querySelector('.js-theme-switcher');
-
-switcher.addEventListener('click', () => {
-  page.classList.toggle('page--hero');
-});
--- src/scripts/main.js
+++
@@ -1,6 +0,0 @@
-import './components/menu';
-import Slider from './components/slider';
-import './components/form';
-import './components/theme-switcher';
-
-Slider();
